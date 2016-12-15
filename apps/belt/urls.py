b54e8658adea9f7c2e20a9f64fae60d9b0f82225
marjan hashemi <mh_n_mg@yahoo.com>
2016-12-14 20:06:02 -0800

    Frontend completed

--- apps/belt/urls.py
+++ apps/belt/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     url(r'^register_process$' , views.register_process),
     url(r'^login_process$' , views.login_process),
     url(r'^books$' , views.success),
+    url(r'^add$' , views.add_book),
+    url(r'^book/1$' , views.add_review), # Change this to parameter of book ID
+    url(r'^user/1$' , views.dashboard), # We will change the ID dude or Marjan will kill you!
+    url(r'^delete_review$' , views.delete_review),
+    url(r'^logout$' , views.logout),
 
 ]
--- apps/belt/views.py
+++ apps/belt/views.py
@@ -19,7 +19,7 @@ def register_process(request):
 	errors =[]
 	if request.method == "POST":
 		result = User.userMgr.register(request.POST['first_name'],request.POST['last_name'],request.POST['email'],request.POST['password'], request.POST['confirm_password'])
-	
+
 		if result[0]==True:
 			request.session['first_name'] = result[1].first_name
 			print result, "*******************************************************"
@@ -44,5 +44,27 @@ def login_process(request):
 		request.session['errors'] = result[1]
 		return redirect('/')
 
+def books(request):
+	return render(request, "belt/success.html")
+
+def add_book(request):
+	return render(request, "belt/add_book.html")
+
+def add_book_process(request):
+	return redirect('books/id')
 
 
+def add_review(request):
+	return render(request, "belt/single_book_review.html")
+
+def add_review_process(request):
+	return redirect('books/id')
+
+def delete_review(request):
+	return redirect('/')
+
+def dashboard(request):
+	return render(request, "belt/dashboard.html")
+
+def logout(request):
+	return redirect('/')
